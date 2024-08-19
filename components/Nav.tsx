d064ab1f1ad96a35b5c9699a8a11d8425c8d054a
mikenegh <mikenegh@gmail.com>
2024-08-19 17:03:50 +0000

    First Commit

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
+import { NAV_LINKS } from '../constants';
 
-const Nav = () => {
-  return <div>Nav</div>;
+const Nav: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        {NAV_LINKS.map((link) => (
+          <li key={link.route}>
+            <Link href={link.route}>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+    { label: 'Home', route: '/' },
+    { label: 'About', route: '/about' },
+    { label: 'Contact', route: '/contact' },
+    { label: 'Products', route: '/product' }
+  ];
+  
