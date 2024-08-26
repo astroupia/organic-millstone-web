51795dd77619e5196fe3acc9b4c8aa828a15e932
mikenegh <mikenegh@gmail.com>
2024-08-26 15:46:39 +0000

    Error Fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,30 +4,47 @@ import React, { useEffect, useState } from 'react';
 import { NAV_LINKS as links } from '@/constants';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Menu, X } from 'lucide-react';
+import {
+  Menu,
+  X,
+} from 'lucide-react';
 import { usePathname } from 'next/navigation';
+
+
 
 const Nav = () => {
   const [nav, setNav] = useState(false);
   const path = usePathname();
   const [scrolled, setScrolled] = useState(false);
 
+
   useEffect(() => {
     const handleScroll = () => {
-      setScrolled(window.scrollY > 0);
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
 
     window.addEventListener('scroll', handleScroll);
 
+    // Clean up the event listener on unmount
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+
+
   return (
-    <div className={`${scrolled ? 'backdrop-blur-xl bg-white/30' : 'bg-white'} flex justify-between items-center w-full h-20 px-4 fixed nav z-[1000]`}>
+    <div className={`${  scrolled ? 'backdrop-blur-xl bg-white/30' : 'bg-white'}  flex justify-between items-center w-full h-20 px-4 fixed nav z-[1000] `}>
       <div className='md:ml-10 ml-5'>
-        <Image src="/assets/images/Om.png" height={70} width={85} alt="logo" />
+       
+         
+            <Image src = "/assets/images/Om.png" height={70} width={85} alt="logo" />
+         
+        
       </div>
 
       <ul className="hidden md:flex">
@@ -36,6 +53,7 @@ const Nav = () => {
             key={label}
             className={`nav-links px-4 cursor-pointer capitalize font-medium hover:scale-105 hover:text-black duration-200 link-underline ${
               path === route ? 'text-green-600' : 'text-black'
+          
             }`}
           >
             <Link href={route}>{label}</Link>
@@ -43,7 +61,10 @@ const Nav = () => {
         ))}
       </ul>
 
-      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 md:hidden">
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 md:hidden"
+      >
         {nav ? <X size={30} /> : <Menu size={30} />}
       </div>
 
@@ -53,7 +74,7 @@ const Nav = () => {
             <li
               key={label}
               className={`px-4 cursor-pointer capitalize py-4 text-base ${
-                path === route ? 'text-green-600' : 'text-gray-500'
+                path === route ? 'green-600' : 'text-gray-500'
               }`}
             >
               <Link onClick={() => setNav(!nav)} href={route}>
