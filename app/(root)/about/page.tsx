e7399e19a35d5cbb5fcd5ba275020b8038332182
Nahom Tegaude <nahomteguade@gmail.com>
2024-09-08 19:04:38 +0300

    Basic Layout Matadata Changed

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import AboutPage from "@/view/AboutPage";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  return <AboutPage />;
+};
 
-export default page
+export default page;
--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ContactForm } from "@/components/ContactForm";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const ContactPage = () => {
+  return <ContactForm />;
+};
 
-export default page
+export default ContactPage;
