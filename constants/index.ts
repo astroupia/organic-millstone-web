4670cf21a98ef09a619dfe14f5bb74ac753f9ca8
Michael <mikenegh@gmail.com>
2024-09-10 23:03:24 +0300

    Links rearranged

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const NAV_LINKS = [
   { label: "Home", route: "/" },
   { label: "About", route: "/about" },
-  { label: "Contact", route: "/contact" },
   { label: "Products", route: "/product" },
+  { label: "Contact", route: "/contact" },
 ];
