7da9130302d4c1d9fd9f8b87ed087d7d6a2fb937
Michael <mikenegh@gmail.com>
2024-09-17 20:04:23 +0300

    start

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com','www.pexels.com','assets.aceternity.com'], // Add the domain here
-      },
+  output: "export",
+  images: {
+    domains: ["images.unsplash.com", "www.pexels.com", "assets.aceternity.com"], // Add the domain here
+  },
 };
 
 export default nextConfig;
