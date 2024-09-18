b0447a9e1a408ded0919323177565f08fe7d7a84
astroupia <103548612+astroupia@users.noreply.github.com>
2024-09-18 13:48:50 +0300

    chore: footer anchor tag changed to Link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Instagram, Youtube, Send } from "lucide-react"; // Using Send icon as a representation for Telegram
+import Link from "next/link";
 
 interface FooterLink {
   label: string;
@@ -19,9 +20,9 @@ const FooterList: React.FC<FooterListProps> = ({ title, links }) => (
     <ul className="text-gray-500 dark:text-gray-400 font-medium">
       {links.map((link, index) => (
         <li className="mb-4" key={index}>
-          <a href={link.href} className="hover:underline">
+          <Link href={link.href} className="hover:underline">
             {link.label}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
@@ -34,7 +35,7 @@ const Footer: React.FC = () => {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a
+            <Link
               href="https://organicmilestone.com/"
               className="flex items-center"
             >
@@ -47,7 +48,7 @@ const Footer: React.FC = () => {
               <span className="self-center text-2xl ml-2 font-semibold whitespace-nowrap dark:text-white">
                 Organic Milestone
               </span>
-            </a>
+            </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <FooterList
@@ -71,33 +72,36 @@ const Footer: React.FC = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
-            <a href="https://organicmilestone.com/" className="hover:underline">
+            <Link
+              href="https://organicmilestone.com/"
+              className="hover:underline"
+            >
               Organic Milestone™
-            </a>
+            </Link>
             . All Rights Reserved. Developed by Sydek
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <a
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <Send className="w-4 h-4" aria-hidden="true" />
               <span className="sr-only">Telegram channel</span>
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
             >
               <Youtube className="w-4 h-4" aria-hidden="true" />
               <span className="sr-only">YouTube channel</span>
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
             >
               <Instagram className="w-4 h-4" aria-hidden="true" />
               <span className="sr-only">Instagram page</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
