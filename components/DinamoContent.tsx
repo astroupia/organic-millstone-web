e715023b0d114ca751a179a5ccb73e2515d5b048
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2024-09-30 22:19:32 +0300

    Update DinamoContent.tsx

--- components/DinamoContent.tsx
+++ components/DinamoContent.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const DnContent = () => {
   return (
     <>
-      {/* First unique div */}
       <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
         <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
           <span className="font-bold text-neutral-700 dark:text-neutral-200">
